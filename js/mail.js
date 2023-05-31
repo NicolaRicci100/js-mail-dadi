c981595a01cdf6c266b0762dfbbca2fbe31818ef
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-31 16:28:32 +0200

    sistemazione mail.js

--- js/mail.js
+++ js/mail.js
@@ -1,1 +1,24 @@
-console.log('JS OK');
+console.log('JS OK MAIL');
+
+// array con le mail valide
+const mails = ['pinco.pallino@gmail.com', 'johnny.beegonny@libero.it', 'larry.poggers@hotmail.com'];
+
+// leggo l'array in console
+console.log(mails);
+
+// prendo dal DOM la mail immessa dall'utente e il bottone
+const mailElement = document.getElementById('my-mail');
+let buttonElement = document.getElementById('my-validation');
+
+buttonElement = addEventListener('click', function(){
+    for(let i = 0; i <= mails.length; i++){
+    console.log(mails[i]);
+
+        if(mails[i] == mailElement){
+            console.log('MAIL VALIDA');
+        } else {
+            console.log('MAIL NON VALIDA');
+        }
+
+    }
+})
---
+++ steps.md
@@ -0,0 +1,10 @@
+# Steps
+## Mail
+- Creo un *array* con alcune mail
+- Chiedo all'utente di inserire una mail
+- **SE** la mail **NON** è tra quelle presenti nell'*array*
+    - mostro messaggio di errore
+- **ALTRIMENTI**
+    - mostro un messaggio che conferma che la mail è corretta
+
+## Dadi
