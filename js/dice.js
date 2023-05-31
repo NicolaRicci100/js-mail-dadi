9e9a43da9e09b4900caf84aae1ac24aceaec781b
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-31 17:30:47 +0200

    aggiungo commenti dado

--- js/dice.js
+++ js/dice.js
@@ -1,18 +1,20 @@
 console.log('JS OK DADI');
 
-
+//collegamento al bottone nel DOM
 const rollElement = document.getElementById('my-dice');
+//lancio i dadi
 rollElement.addEventListener('click', function(){
+    
     const userDice = parseInt(Math.floor(Math.random() * 6) + 1);
-    console.log('Umano ' + userDice);
+    console.log('Umano ' + userDice); //risulato dado UMANO
     const cpuDice = parseInt(Math.floor(Math.random() * 6) + 1);
-    console.log('Computer ' + cpuDice);
+    console.log('Computer ' + cpuDice); //risultato dado CPU
 
-    if(userDice > cpuDice){
+    if(userDice > cpuDice){             //se UMANO batte CPU
         console.log('vince l\'umano');
-    } else if(userDice == cpuDice){
+    } else if(userDice == cpuDice){     //se i risulati sono uguali
         console.log('pareggio');
-    } else{
+    } else{                             //se CPU batte UMANO
         console.log('vince il computer');
     }
 
