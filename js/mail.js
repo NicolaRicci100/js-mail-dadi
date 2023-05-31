312078e6208174613dff4ebac2848f3f2368eafe
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-31 18:01:45 +0200

    sistemazione for e if di mail.js

--- js/mail.js
+++ js/mail.js
@@ -11,12 +11,11 @@ const mailElement = document.getElementById('my-mail');
 const buttonElement = document.getElementById('my-validation');
 
 buttonElement.addEventListener('click', function(){
-    for(let i = 0; i <= mails.length; i++){
-
+    let text = ('MAIL NON VALIDA');
+    for(let i = 0; i < mails.length && text == ('MAIL NON VALIDA'); i++){
         if(mails[i] == mailElement.value){
-            console.log(mails[i] + ' è una MAIL VALIDA');
-        } else{
-            console.log('MAIL NON VALIDA');
-        }
+            text = (mails[i] + ' è una MAIL VALIDA');
+        }     
     }
+    console.log(text);
 })
